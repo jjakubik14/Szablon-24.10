7310f2b1a0783d1e62a2e8967e3657ba27c337da
verdun <34871541+patryk-pszeniczny@users.noreply.github.com>
2025-10-24 11:17:35 +0200

    Update GameForm.cpp

--- Szablon/GameForm.cpp
+++ Szablon/GameForm.cpp
@@ -1,2 +1,17 @@
 #include "GameForm.h"
 
+namespace Szablon {
+	GameForm::GameForm(User* user) {
+		InitializeComponent();
+		this->user = user;
+
+		this->initName();
+	}
+	User* GameForm::getUser() {
+		return this->user;
+	}
+	void GameForm::initName() {
+		this->kto_gra_okno->Text = gcnew System::String(this->user->getName().c_str());
+	}
+
+}
